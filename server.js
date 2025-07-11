ff6b67ef3d708470b1ed94593c40cd48d63388e9
nishthakore <nthakore12@gmail.com>
2025-07-11 10:47:23 +0530

    Update server.js

--- server.js
+++ server.js
@@ -15,7 +15,11 @@ connectDB();
 const app = express();
 
 //middelwares
-app.use(cors());
+// app.use(cors());
+app.use(cors({
+  origin: "https://online-art-gallery.vercel.app",
+  credentials: true,
+}));
 app.use(express.json());
 app.use(morgan("dev"));
 
